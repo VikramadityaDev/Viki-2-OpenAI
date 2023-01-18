a4252ba18b078a518d56047737630f5b8a2f45d4
Vikramaditya <63139007+VikramadityaDev@users.noreply.github.com>
2023-01-18 21:15:00 +0530

    Update constants.dart

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,5 +1,5 @@
 // ignore_for_file: non_constant_identifier_names
 
-String OPEN_API_KEY = 'ENTER_YOUR_API_KEY_HERE';
+String OPEN_API_KEY = 'OPEN_AI_API_KEY_HERE';
 
 String BASE_URL = 'https://api.openai.com/v1/';
