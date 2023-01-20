9c0d125ecbc250405d1127109318e100c1763e12
Vikramaditya <63139007+VikramadityaDev@users.noreply.github.com>
2023-01-20 13:36:08 +0530

    Removed API Key

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,5 +1,5 @@
 // ignore_for_file: non_constant_identifier_names
 
-String OPEN_API_KEY = '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';
+String OPEN_API_KEY = 'OPEN_AI_API_KEY_HERE';
 
 String BASE_URL = 'https://api.openai.com/v1/';
